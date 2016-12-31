cea6175f1fcd8a2ef562f5d12bc8d8ba25a835bb
joshua906 <joshuamatthews906@yahoo.com>
2016-12-31 16:17:23 -0600

    menu changes and themesupport added

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ add_action( 'wp_enqueue_scripts', 'portfolio_theme_styles');
 function portfolio_theme_js() {
 	  wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.js' );
   	  wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js' );
+	  wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-3.1.1.min.js (1)' );
 	
 
 }
@@ -24,6 +25,8 @@ add_action( 'wp_enqueue_scripts', 'portfolio_theme_js');
 
 
 //add theme support
+add_theme_support('custom-background');
+add_theme_support('post-thumbnail');
 add_theme_support('menus');
 
 
---
+++ page-portfolio.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+
+<h1>Portfolio</h1>
+
+
+<?php get_footer(); ?>
