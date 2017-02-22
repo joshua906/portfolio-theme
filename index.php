9fa889ba6eefb856f21c51268fc3775ae309216f
joshua906 <joshuamatthews906@yahoo.com>
2017-02-21 19:31:10 -0600

    blog set up

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php get_header(); ?>
+<!-- blog header -->
+<div class="main-hero">
+			<div class="container">
+			<div class="row">
+				<div class="col-md-9">
+					<div class="">
+
+						<h1 class="small-font-family module">Insight and News</h1>
+						
+
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
 
 	<?php 
 	
@@ -6,8 +21,16 @@
 		
 		while( have_posts() ): the_post(); ?>
 			
-			<?php get_template_part('content',get_post_format()); ?>
-		
+			
+			<div class="blog-layout">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6">
+						<?php get_template_part('content',get_post_format()); ?>
+						</div>
+					</div>
+				</div>
+			</div>
 		<?php endwhile;
 		
 	endif;
